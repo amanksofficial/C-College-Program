dd110b20c0fb3595abed335834f11dd19752ec64
amanksofficial <116274829+amanksofficial@users.noreply.github.com>
2022-12-12 08:47:11 +0530

    Create area of triangle.c

---
+++ area of triangle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+ double base, height, area;
+  double a = 0.5;
+ printf("Enter the base of the triangle :-");
+ scanf("%lf", &base);
+ 
+printf("Enter the height of the triangle :-");
+ scanf("%lf", &height);
+ 
+ area = a*base*height;
+printf("the area of the tringle is%lf:-", area);
+
+    return 0;
+}
